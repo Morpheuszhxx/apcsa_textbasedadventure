3beea6a3a7c22659a152810e1383114c2fb4c99b
Morpheuszhxx <72705937+Morpheuszhxx@users.noreply.github.com>
2025-10-30 15:49:35 -0700

    second commit
    includes game framework

--- TextBasedAdventure/src/App.java
+++ TextBasedAdventure/src/App.java
@@ -1,7 +1,5 @@
 public class App {
-    public static void main(String[] args) {
-        System.out.println("This is Luna Celes' Dad");
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
-
-
